4c4682991d5281fc0ce6ffcb585bb7098bc3d93f
Jonathan Lee <j47lee@gmail.com>
2015-10-15 14:36:29 -0700

    setup object structure

--- greetr.js
+++ greetr.js
@@ -1,5 +1,30 @@
 (function(global, jquery){
 
+  //calls new Greetr.init fn to create object
+  var Greetr = function(firstname, lastname, language){
+    return new Greetr.init(firstname, lastname, language);
+  }
 
+  Greetr.prototype = {}
+
+  //object structure
+  Greetr.init = function(firstname, lastname, language){
+    var self = this;
+    self.firstname = firstname || '';
+    self.lastname = lastname || '';
+    self.language = language || 'en';
+  }
+
+  //points init to initial object
+  Greetr.init.prototype = Greetr.prototype
 
 })(window, $) //end IIFE
+
+
+
+// function Person(){
+//   this.firstname;
+//   this.lastname;
+//   this.language
+// }
+// var g = new Person()
